90990475d45eec9ecc5975e7c8699ca6e9bbbb1e
tripp <me@trippsmith.com>
2026-01-13 21:49:40 -0500

    Import extension exports in package init

--- python/bmssp/__init__.py
+++ python/bmssp/__init__.py
@@ -1,14 +1,23 @@
 """BMSSP: Fast single-source shortest paths for grid and pipeline networks."""
 
-from .graph import Graph
-from .sssp import sssp, SSSPResult, reconstruct_path, multi_sink_costs
+from _bmssp import *  # noqa: F401,F403
 
-__all__ = [
-    "Graph",
-    "sssp",
-    "SSSPResult",
-    "reconstruct_path",
-    "multi_sink_costs",
-]
+from .graph import Graph
+from .sssp import SSSPResult, multi_sink_costs, reconstruct_path, sssp
+
+_bmssp_all = list(globals().get("__all__", []))
+
+__all__ = list(
+    dict.fromkeys(
+        _bmssp_all
+        + [
+            "Graph",
+            "sssp",
+            "SSSPResult",
+            "reconstruct_path",
+            "multi_sink_costs",
+        ]
+    )
+)
 
 __version__ = "0.1.0"
