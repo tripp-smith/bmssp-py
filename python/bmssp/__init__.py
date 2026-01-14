fafb20e838fbc6cb6858e2bb92cf3991c74119f8
tripp <me@trippsmith.com>
2026-01-13 22:13:37 -0500

    Restore bmssp wrapper exports

--- python/bmssp/__init__.py
+++ python/bmssp/__init__.py
@@ -1,1 +1,23 @@
+"""BMSSP: Fast single-source shortest paths for grid and pipeline networks."""
+
 from _bmssp import *  # re-export extension API
+
+from .graph import Graph
+from .sssp import SSSPResult, multi_sink_costs, reconstruct_path, sssp
+
+_bmssp_all = list(globals().get("__all__", []))
+
+__all__ = list(
+    dict.fromkeys(
+        _bmssp_all
+        + [
+            "Graph",
+            "sssp",
+            "SSSPResult",
+            "reconstruct_path",
+            "multi_sink_costs",
+        ]
+    )
+)
+
+__version__ = "0.1.0"
